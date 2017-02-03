22f242bead6b85e7ea7d1acea5a2dd5d1238360c
Manjunath-SS <mssangashetty@gmail.com>
2017-02-03 13:30:00 +0530

    LoginForm

--- lgin/forms.py
+++ lgin/forms.py
@@ -1,6 +1,15 @@
 from django import forms
 from django.utils.safestring import mark_safe
+from django.template import Template
 
-class NameForm(forms.Form):
-    usr = forms.CharField(label='Username:', max_length=100)
-    pwd = forms.CharField(label=mark_safe('<br>Password:'), max_length=10,)
+
+
+class LoginForm(forms.Form):
+    usr = forms.CharField(label='Username:', max_length=10)
+    pwd = forms.CharField(label=mark_safe('<br>Password:'), max_length=10,widget=forms.PasswordInput)
+    keep_logged = forms.BooleanField(required=False, label=mark_safe('<br><br>Keep me logged in '))
+
+    buttons = Template("""
+        <button class="waves-effect waves-teal btn-flat">Register</button>
+        <button class="waves-effect waves-light btn" type="submit">Login</button>
+    """)
--- lgin/views.py
+++ lgin/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
-from .forms import NameForm
+from .forms import LoginForm
 from django.template import Context, loader
 from .models import CamsLogin, FacultyList, ClassTchr
 
@@ -39,7 +39,7 @@ def login(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
+        form = LoginForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
@@ -56,5 +56,5 @@ def login(request):
                 return lerror(request)
     # if a GET (or any other method) we'll create a blank form
     else:
-        form = NameForm()
+        form = LoginForm()
     return render(request, 'loginform.html', {'form': form})
